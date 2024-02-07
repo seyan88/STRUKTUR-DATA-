ef9932d5f9c05998099c08451bce88361e405cc7
seyan88 <148548066+seyan88@users.noreply.github.com>
2024-02-07 15:35:38 +0700

    SISTEM_PEMESANAN_TIKET_BIOSKOP

---
+++ APLIKASI_PEMESANAN_TIKET_BIOSKOP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_pemesanan_tiket_bioskop;
+
+/**
+ *
+ * @author devninngel
+ */
+public class APLIKASI_PEMESANAN_TIKET_BIOSKOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
